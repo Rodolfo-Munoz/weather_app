890b9acbfd21348bc010fd651befafa8e7fb1f17
mr.verloc <mr.verloc@gmail.com>
2020-08-25 18:29:35 +0100

    Prevents duplicate cities to be added

--- weather/views.py
+++ weather/views.py
@@ -4,17 +4,23 @@ from .models import City
 from .forms import CityForm
 
 
-
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=ab27c6408d2bf2ab57f572629eef037a'
 
-    if request.method == 'POST':
-        try:
-            form = CityForm(request.POST)
-            form.save()
-        except KeyError:
-            print('That City does not exist in our database')
+    err_msg = ''
 
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+
+        if form.is_valid():
+            # in this variable we hold the city entered by the user
+            new_city = form.cleaned_data['name']
+            # if the existing_city_count is 0, then the city is not duplicated
+            existing_city_count = City.objects.filter(name=new_city).count()
+            if existing_city_count == 0:
+                form.save()
+            else:
+                err_msg = 'City already exists in the database'
 
     form = CityForm()
 
